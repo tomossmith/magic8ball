976141a3b70bc4a1d9344cd8effcbc064ad22651
Tom Smith <tomossmith86@icloud.com>
2023-01-15 23:01:28 +0000

    Began building HTML to work with JS

--- assets/js/8ball.js
+++ assets/js/8ball.js
@@ -1,38 +1,48 @@
-let userName = ;
-let userQuestion = ;
-let randomNumber = Math.floor(Math.random() * 8);
-let eightBall = '';
+submit.addEventListener("click", function () {
+    let userName = document.getElementsById(userName);;
+    let userQuestion = document.getElementsById(userQuestion);;
+    let submit = document.getElementsById(submit)
+    if (userQuestion.value.length < 1) {
+        alert('Enter a question!');
+    } else {
+        eightBall.innerText = "";
+        let randomNumber = Math.floor(Math.random() * 8);
+        switch (randomNumber) {
+            case 0:
+                eightBall = "It is certain";
+                break;
+            case 1:
+                z
+                eightBall = "It is decidedly so";
+                break;
+            case 2:
+                eightBall = "Reply hazy try again";
+                break;
+            case 3:
+                eightBall = "Cannot predict now";
+                break;
+            case 4:
+                eightBall = "Do not count on it";
+                break;
+            case 5:
+                eightBall = "My sources say no";
+                break;
+            case 6:
+                eightBall = "Outlook not so good";
+                break;
+            case 7:
+                eightBall = "Signs point to yes";
+                break;
+        }
+        answer.innerText = answers[eightBall];
+    }
+});
 
-userName ? console.log(`Hello ${userName}!`) : console.log("Hello!");
+//userName ? console.log(`Hello ${userName}!`) : console.log("Hello!");
 
-console.log(`${userName}\'s question to the Magic 8 Ball was: ${userQuestion}`);
+//console.log(`${userName}\'s question to the Magic 8 Ball was: ${userQuestion}`);
 
 
-switch(randomNumber) {
-  case 0:
-    eightBall = "It is certain";
-    break;
-  case 1:
-    eightBall = "It is decidedly so";
-    break;
-  case 2:
-    eightBall = "Reply hazy try again";
-    break;
-  case 3:
-    eightBall = "Cannot predict now";
-    break;
-  case 4:
-    eightBall = "Do not count on it";
-    break;
-  case 5:
-    eightBall = "My sources say no";
-    break;
-  case 6:
-    eightBall = "Outlook not so good";
-    break;
-  case 7:
-    eightBall = "Signs point to yes";
-    break;
-}
 
-console.log(`The Magic 8 Ball Says: ${eightBall}!`);
+
+//console.log(`The Magic 8 Ball Says: ${eightBall}!`);
